33976c12c90cef9a508a0ab5770b40e895d396e0
x.karimjonov <khtkarimzhonov@gmail.com>
2023-03-14 19:13:29 +0500

    fixes

--- karer_web/settings.py
+++ karer_web/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9mmt@ovupohw7gno$j!lv^^k(+l9q7g_dl907wbk4(q%7!v_v+'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.getenv('DEBUG', True))
 
 ALLOWED_HOSTS = [
     'karer.keyman24.ru',
